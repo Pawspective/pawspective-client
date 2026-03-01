2dd77910eab7d839d269fa2fe018ea5ab5310491
Egor Maximov <egoalemaksimov@edu.hse.ru>
2026-03-01 18:49:43 +0300

    fix: delete flags for cppcheck

--- manage.py
+++ manage.py
@@ -171,9 +171,6 @@ def cppcheck_lint():
             "--project=build-debug/compile_commands.json",
             "--file-filter=src/*",
             "--file-filter=include/*",
-            "-I", "include",
-            "--suppress=normalCheckLevelMaxBranches",
-            "--suppress=checkersReport"
         ]
         
         if pathlib.Path(".cppcheck_suppressions").exists():
