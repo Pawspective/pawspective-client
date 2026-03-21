06bc2f7bb93e04ffc14ad329b59d0fc8778fa7ed
FlintWithBlackCrown <92873054+Granzh@users.noreply.github.com>
2026-03-22 00:27:45 +0300

    feat: add animal_service (#66)
    * feat: add animal_service
    
    * feat: add new handler

---
+++ include/services/animal_service.hpp
@@ -0,0 +1,60 @@
+#pragma once
+
+#include <QJsonArray>
+#include <QJsonObject>
+#include <QList>
+#include <QObject>
+
+#include "models/animal_dto.hpp"
+#include "models/animal_filter_dto.hpp"
+#include "models/animal_register_dto.hpp"
+#include "models/animal_update_dto.hpp"
+#include "services/errors.hpp"
+#include "services/i_network_client.hpp"
+
+namespace pawspective::services {
+
+class AnimalService : public QObject {
+    Q_OBJECT
+public:
+    explicit AnimalService(INetworkClient& networkClient, QObject* parent = nullptr);
+
+    void getAnimals(const models::AnimalFilterDTO& filter);
+    void getAnimal(qint64 id);
+    void createAnimal(const models::AnimalRegisterDTO& dto);
+    void updateAnimal(qint64 id, const models::AnimalUpdateDTO& dto);
+    void getAnimalFilters();
+    void getAnimalsByOrganization(qint64 organizationId);
+
+signals:
+    void getAnimalsSuccess(const QList<models::AnimalDTO>& animals);
+    void getAnimalSuccess(const models::AnimalDTO& animal);
+    void createAnimalSuccess(const models::AnimalDTO& animal);
+    void updateAnimalSuccess(const models::AnimalDTO& animal);
+    void getAnimalFiltersSuccess(const models::AnimalFilterDTO& filters);
+    void getAnimalsByOrganizationSuccess(const QList<models::AnimalDTO>& animals);
+
+    void getAnimalsFailed(QSharedPointer<services::BaseError> error);
+    void getAnimalFailed(QSharedPointer<services::BaseError> error);
+    void createAnimalFailed(QSharedPointer<services::BaseError> error);
+    void updateAnimalFailed(QSharedPointer<services::BaseError> error);
+    void getAnimalFiltersFailed(QSharedPointer<services::BaseError> error);
+    void getAnimalsByOrganizationFailed(QSharedPointer<services::BaseError> error);
+
+private:
+    void handleError(QNetworkReply& reply, std::function<void(QSharedPointer<BaseError>)> onError);
+    void handleSuccess(
+        QNetworkReply& reply,
+        std::function<void(const QJsonObject&)> onSuccess,
+        std::function<void(QSharedPointer<BaseError>)> onError
+    );
+    void handleSuccessArray(
+        QNetworkReply& reply,
+        std::function<void(const QJsonArray&)> onSuccess,
+        std::function<void(QSharedPointer<BaseError>)> onError
+    );
+
+    INetworkClient& m_networkClient;
+};
+
+}  // namespace pawspective::services
---
+++ src/services/animal_service.cpp
@@ -0,0 +1,276 @@
+#include "services/animal_service.hpp"
+
+#include <QJsonArray>
+#include <QJsonDocument>
+#include <QJsonObject>
+#include <QJsonParseError>
+#include <QList>
+#include <QNetworkReply>
+#include <QSharedPointer>
+#include <QUrl>
+#include <QUrlQuery>
+
+#include "models/animal_dto.hpp"
+#include "models/animal_enums.hpp"
+#include "models/animal_filter_dto.hpp"
+#include "services/errors.hpp"
+#include "services/i_network_client.hpp"
+
+namespace pawspective::services {
+
+AnimalService::AnimalService(INetworkClient& networkClient, QObject* parent)
+    : QObject(parent), m_networkClient(networkClient) {}
+
+void AnimalService::handleError(QNetworkReply& reply, std::function<void(QSharedPointer<BaseError>)> onError) {
+    QJsonParseError parseError;
+    QByteArray data = reply.readAll();
+    QJsonDocument doc = QJsonDocument::fromJson(data, &parseError);
+
+    if (parseError.error != QJsonParseError::NoError) {
+        onError(
+            QSharedPointer<BaseError>(new ClientJsonParseError(
+                QString("JSON parse error at %1: %2").arg(parseError.offset).arg(parseError.errorString())
+            ))
+        );
+        return;
+    }
+
+    if (doc.isObject()) {
+        auto error = ErrorFactory::createError(doc.object());
+        onError(QSharedPointer<BaseError>(std::move(error)));
+    } else {
+        onError(QSharedPointer<BaseError>(new UnknownError("Unknown error occurred")));
+    }
+}
+
+void AnimalService::handleSuccess(
+    QNetworkReply& reply,
+    std::function<void(const QJsonObject&)> onSuccess,
+    std::function<void(QSharedPointer<BaseError>)> onError
+) {
+    QJsonParseError parseError;
+    QByteArray data = reply.readAll();
+    try {
+        QJsonDocument doc = QJsonDocument::fromJson(data, &parseError);
+
+        if (parseError.error != QJsonParseError::NoError) {
+            onError(
+                QSharedPointer<BaseError>(new ClientJsonParseError(
+                    QString("JSON parse error at %1: %2").arg(parseError.offset).arg(parseError.errorString())
+                ))
+            );
+            return;
+        }
+
+        onSuccess(doc.object());
+    } catch (const std::exception& e) {
+        onError(QSharedPointer<BaseError>(new ClientJsonParseError(QString(e.what()))));
+    }
+}
+
+void AnimalService::handleSuccessArray(
+    QNetworkReply& reply,
+    std::function<void(const QJsonArray&)> onSuccess,
+    std::function<void(QSharedPointer<BaseError>)> onError
+) {
+    QJsonParseError parseError;
+    QByteArray data = reply.readAll();
+    try {
+        QJsonDocument doc = QJsonDocument::fromJson(data, &parseError);
+
+        if (parseError.error != QJsonParseError::NoError) {
+            onError(
+                QSharedPointer<BaseError>(new ClientJsonParseError(
+                    QString("JSON parse error at %1: %2").arg(parseError.offset).arg(parseError.errorString())
+                ))
+            );
+            return;
+        }
+
+        if (!doc.isArray()) {
+            onError(QSharedPointer<BaseError>(new UnknownError("Expected JSON array in response")));
+            return;
+        }
+
+        onSuccess(doc.array());
+    } catch (const std::exception& e) {
+        onError(QSharedPointer<BaseError>(new ClientJsonParseError(QString(e.what()))));
+    }
+}
+
+void AnimalService::getAnimals(const models::AnimalFilterDTO& filter) {
+    QUrl url("/animals");
+    QUrlQuery query;
+
+    if (filter.breeds) {
+        for (int64_t breedId : *filter.breeds) {
+            query.addQueryItem("breedId", QString::number(breedId));
+        }
+    }
+    if (filter.animalTypes) {
+        for (models::AnimalType type : *filter.animalTypes) {
+            query.addQueryItem("animalType", models::toApiString(type));
+        }
+    }
+    if (filter.sizes) {
+        for (models::AnimalSize size : *filter.sizes) {
+            query.addQueryItem("size", models::toApiString(size));
+        }
+    }
+    if (filter.genders) {
+        for (models::AnimalGender gender : *filter.genders) {
+            query.addQueryItem("gender", models::toApiString(gender));
+        }
+    }
+    if (filter.careLevels) {
+        for (models::CareLevel level : *filter.careLevels) {
+            query.addQueryItem("careLevel", models::toApiString(level));
+        }
+    }
+    if (filter.colors) {
+        for (models::AnimalColor color : *filter.colors) {
+            query.addQueryItem("color", models::toApiString(color));
+        }
+    }
+    if (filter.goodWiths) {
+        for (models::GoodWith goodWith : *filter.goodWiths) {
+            query.addQueryItem("goodWith", models::toApiString(goodWith));
+        }
+    }
+    if (filter.ageLte) {
+        query.addQueryItem("ageLte", QString::number(*filter.ageLte));
+    }
+    if (filter.ageGte) {
+        query.addQueryItem("ageGte", QString::number(*filter.ageGte));
+    }
+
+    if (!query.isEmpty()) {
+        url.setQuery(query);
+    }
+
+    m_networkClient.get(
+        url,
+        [this](QNetworkReply& reply) {
+            handleSuccessArray(
+                reply,
+                [this](const QJsonArray& arr) {
+                    QList<models::AnimalDTO> animals;
+                    for (const auto& item : arr) {
+                        animals.append(models::AnimalDTO::fromJson(item.toObject()));
+                    }
+                    emit getAnimalsSuccess(animals);
+                },
+                [this](QSharedPointer<BaseError> error) { emit getAnimalsFailed(error); }
+            );
+        },
+        [this](QNetworkReply& reply) {
+            handleError(reply, [this](QSharedPointer<BaseError> error) { emit getAnimalsFailed(error); });
+        }
+    );
+}
+
+void AnimalService::getAnimal(qint64 id) {
+    m_networkClient.get(
+        QUrl(QString("/animals/%1").arg(id)),
+        [this](QNetworkReply& reply) {
+            handleSuccess(
+                reply,
+                [this](const QJsonObject& obj) {
+                    models::AnimalDTO animal = models::AnimalDTO::fromJson(obj);
+                    emit getAnimalSuccess(animal);
+                },
+                [this](QSharedPointer<BaseError> error) { emit getAnimalFailed(error); }
+            );
+        },
+        [this](QNetworkReply& reply) {
+            handleError(reply, [this](QSharedPointer<BaseError> error) { emit getAnimalFailed(error); });
+        }
+    );
+}
+
+void AnimalService::createAnimal(const models::AnimalRegisterDTO& dto) {
+    const QJsonDocument doc(dto.toJson());
+
+    m_networkClient.post(
+        QUrl("/animals"),
+        doc.toJson(QJsonDocument::Compact),
+        [this](QNetworkReply& reply) {
+            handleSuccess(
+                reply,
+                [this](const QJsonObject& obj) {
+                    models::AnimalDTO animal = models::AnimalDTO::fromJson(obj);
+                    emit createAnimalSuccess(animal);
+                },
+                [this](QSharedPointer<BaseError> error) { emit createAnimalFailed(error); }
+            );
+        },
+        [this](QNetworkReply& reply) {
+            handleError(reply, [this](QSharedPointer<BaseError> error) { emit createAnimalFailed(error); });
+        }
+    );
+}
+
+void AnimalService::updateAnimal(qint64 id, const models::AnimalUpdateDTO& dto) {
+    const QJsonDocument doc(dto.toJson());
+
+    m_networkClient.put(
+        QUrl(QString("/animals/%1").arg(id)),
+        doc.toJson(QJsonDocument::Compact),
+        [this](QNetworkReply& reply) {
+            handleSuccess(
+                reply,
+                [this](const QJsonObject& obj) {
+                    models::AnimalDTO animal = models::AnimalDTO::fromJson(obj);
+                    emit updateAnimalSuccess(animal);
+                },
+                [this](QSharedPointer<BaseError> error) { emit updateAnimalFailed(error); }
+            );
+        },
+        [this](QNetworkReply& reply) {
+            handleError(reply, [this](QSharedPointer<BaseError> error) { emit updateAnimalFailed(error); });
+        }
+    );
+}
+
+void AnimalService::getAnimalFilters() {
+    m_networkClient.get(
+        QUrl("/animals/filters"),
+        [this](QNetworkReply& reply) {
+            handleSuccess(
+                reply,
+                [this](const QJsonObject& obj) {
+                    models::AnimalFilterDTO filters = models::AnimalFilterDTO::fromJson(obj);
+                    emit getAnimalFiltersSuccess(filters);
+                },
+                [this](QSharedPointer<BaseError> error) { emit getAnimalFiltersFailed(error); }
+            );
+        },
+        [this](QNetworkReply& reply) {
+            handleError(reply, [this](QSharedPointer<BaseError> error) { emit getAnimalFiltersFailed(error); });
+        }
+    );
+}
+
+void AnimalService::getAnimalsByOrganization(qint64 organizationId) {
+    m_networkClient.get(
+        QUrl(QString("/org/%1/animals").arg(organizationId)),
+        [this](QNetworkReply& reply) {
+            handleSuccessArray(
+                reply,
+                [this](const QJsonArray& arr) {
+                    QList<models::AnimalDTO> animals;
+                    for (const auto& item : arr) {
+                        animals.append(models::AnimalDTO::fromJson(item.toObject()));
+                    }
+                    emit getAnimalsByOrganizationSuccess(animals);
+                },
+                [this](QSharedPointer<BaseError> error) { emit getAnimalsByOrganizationFailed(error); }
+            );
+        },
+        [this](QNetworkReply& reply) {
+            handleError(reply, [this](QSharedPointer<BaseError> error) { emit getAnimalsByOrganizationFailed(error); });
+        }
+    );
+}
+
+}  // namespace pawspective::services
